e7df3e6a0ef3ca430e5d75a0452276755b9948e5
j0sh1x <aljoshua.hell@gmail.com>
2017-12-16 00:51:57 +0100

    various fixes to make it boot
    Change-Id: I1f7f9ccfc17df1b1261cdc59947ef2383d4af9a9

--- device.mk
+++ device.mk
@@ -163,10 +163,19 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
+PRODUCT_PACKAGES += \
+        android.hardware.gnss@1.0-impl \
+        android.hardware.gnss@1.0-service
+
 # Keys
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:/system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:/system/usr/keylayout/sec_touchscreen.kl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
